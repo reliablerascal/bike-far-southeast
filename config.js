1f749b4470f2b580d38edd87d24c166d9be565da
Rob Reid <reliablerascal@gmail.com>
2023-08-31 10:23:19 -0500

    updated positioning of map container

--- config.js
+++ config.js
@@ -11,15 +11,36 @@ var config = {
     theme: 'dark',
     use3dTerrain: false, //set true for enabling 3D maps.
     auto: false,
-    title: 'PROTOTYPE- Far South Side Bike Access',
-    subtitle: 'Chicago\'s Southeast side possesses a wealth of waterfront and park space, but bikers can\'t yet safely access it.',
-    byline: 'By Rob Reid | 31 August',
-    footer: 'See GitHub repository for methodology and data.<br>Created based on the <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
-    leadPhoto: 'https://reliablerascal.github.io/bike-far-southeast/images/calumet-park-beach-fence-16-9.jpg',
+    // title: 'PROTOTYPE- Far South Side Bike Access',
+    // subtitle: 'Chicago\'s Southeast side possesses a wealth of waterfront and park space, but bikers can\'t yet safely access it.',
+    // byline: 'By Rob Reid | 31 August',
+    // footer: 'See GitHub repository for methodology and data.<br>Created based on the <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
+    // leadPhoto: 'https://reliablerascal.github.io/bike-far-southeast/images/calumet-park-beach-fence-16-9.jpg',
     // leadParagraph: 'Hegewisch and East Side, Chicago’s southeasternmost neighborhoods, possess an abundance of park space, including Eggers Grove, Wolf Lake, Calumet Park, and Hegewisch Marsh Park. The I-90 expressway and major streets including 106th Street and Ewing Avenue facilitate automobile access for far away visitors. But these resources remain underutilized, in part because access remains treacherous for local residents– especially cyclists.',
     chapters: [
         {
-            id: 'hide-header',
+            id: 'exit-map-top',
+            alignment: 'left',
+            hidden: true,
+            //image: 'https://reliablerascal.github.io/bike-far-southeast/images/calumet-park-beach.jpg',
+            //description: 'This section should restore map.',
+            location: {
+                center: [-87.52929693758914,
+                    41.714936581261895],
+                zoom: 14,
+                pitch: 0.00,
+                bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: 'exitMapTop',
+            onChapterEnter: [                  
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'enter-map-top',
             alignment: 'left',
             hidden: true,
             //image: 'https://reliablerascal.github.io/bike-far-southeast/images/calumet-park-beach.jpg',
@@ -46,7 +67,7 @@ var config = {
         
         
         {
-            id: 'header-photo',
+            id: 'header-spacer',
             alignment: 'full',
             hidden: true,
             //title: '',
